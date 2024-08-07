95b6609cf4b27ae4cc8aa70e0ca8a5c5de3cf773
Mike Sheets <wmichaelsheets@gmx.com>
2024-08-06 22:45:28 -0400

    corrected a bad array reference in PotteryList.js

--- src/scripts/PotteryList.js
+++ src/scripts/PotteryList.js
@@ -8,7 +8,7 @@ for (const pottery of potteryArray) {
     potteryHTMLString += `<section class="pottery" id="pottery--${pottery.id}">
   <h2 class="pottery__shape">${pottery.shape}</h2>
   <div class="pottery__properties">
-    Item weighs ${pottery.weight} grams and is ${pottery.weight} cm in height
+    Item weighs ${pottery.weight} grams and is ${pottery.height} cm in height
   </div>
   <div class="pottery__price">Price is $${pottery.price}</div>
 </section>`
